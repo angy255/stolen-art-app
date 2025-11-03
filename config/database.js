04d3c6cebd9e3dba651cebc3212a4ba468633e15
Angelica Matos <angelicamatos255@gmail.com>
2025-11-03 01:36:01 -0500

    add updated files

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://angymacodes_db_user:TioiyWRMwO2gwZM4@cluster0.5wlv0gm.mongodb.net/artheist?appName=Cluster0",
+    'dbName': 'art'
+};
+
+
